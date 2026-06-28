9656d5943e23f117080da847780d8f20e2656153
SAAD <moussaifsaad4@gmail.com>
2026-06-28 14:16:43 +0100

    add carrirere content

--- src/app/pages/carrieres/carrieres.component.ts
+++ src/app/pages/carrieres/carrieres.component.ts
@@ -1,11 +1,44 @@
+// carrieres.component.ts
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-carrieres',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterModule, LucideAngularModule],
   templateUrl: './carrieres.component.html',
-  styleUrl: './carrieres.component.scss',
+  styleUrl: './carrieres.component.scss'
 })
 export class CarrieresComponent {
 
+  form = {
+    prenom: '',
+    nom: '',
+    email: '',
+    telephone: '',
+    poste: '',
+    experience: '',
+    technologies: '',
+    portfolio: '',
+    lettre: ''
+  };
+
+  // ← appelé depuis les boutons "Postuler"
+  setPoste(poste: string) {
+    this.form.poste = poste;
+  }
+
+  submitForm() {
+    console.log('Candidature envoyée :', this.form);
+    // TODO: appel API ou EmailJS
+    alert('Candidature envoyée ! Nous vous répondrons sous 48h.');
+    this.form = {
+      prenom: '', nom: '', email: '', telephone: '',
+      poste: '', experience: '', technologies: '',
+      portfolio: '', lettre: ''
+    };
+  }
 }
